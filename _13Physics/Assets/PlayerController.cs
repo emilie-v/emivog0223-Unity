65e128235c2b42e4c690a7352593e24786f55de1
Emilie <emivog0223@skola.goteborg.se>
2020-11-02 16:31:17 +0100

    Lesson13

---
+++ _13Physics/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float jumpForce = 5f;
+
+    private Rigidbody2D rb2d;
+    bool canJump = true;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(x, 0);
+        rb2d.AddForce(movement * speed);
+
+        if (Input.GetAxis("Jump") > 0 && canJump)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = false;
+        }
+    }
+}
