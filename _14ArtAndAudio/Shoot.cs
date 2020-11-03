8468ad13d9a5cc393097fc28e718cf3b57c9f732
Emilie <emivog0223@skola.goteborg.se>
2020-11-03 16:08:21 +0100

    created

---
+++ _14ArtAndAudio/Shoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioVariation : MonoBehaviour
+{
+	//An array with the different audio clips.
+	public AudioClip[] audioClips;
+
+	//Reference to our AudioSource
+	AudioSource audioSource;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+
+		if (audioClips.Length > 0)
+		{
+			//Select a random clip from our array with clips
+			int rndIndex = Random.Range(0, audioClips.Length);
+			audioSource.clip = audioClips[rndIndex];
+
+			audioSource.pitch = Random.Range(0.8f, 1.2f);
+
+			audioSource.Play();
+		}
+	}
+}
+
+public class Shoot : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
