1e5dbd6cc6ff1171cb7ad39f97c85b1cd5dea33b
Emilie <emivog0223@skola.goteborg.se>
2020-11-29 15:10:27 +0100

    Started Udemy

---
+++ Udemy/Assets/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Pick a number, don't tell me what it is!");
+        Debug.Log("The highest is 1000, the lowest is 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
